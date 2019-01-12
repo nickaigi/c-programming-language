8963a284cc28284f4183a18c11a0ddf3c5e2c412
Nickson Kaigi <nickaigi@gmail.com>
2019-01-12 17:11:07 +0300

    using a recursive approach

--- raise-to-power.c
+++ raise-to-power.c
@@ -10,9 +10,22 @@ int main(){
     return 0;
 }
 
+/* I thought that this should be a recursive problem 
+ * since x^y = x * x^(y-1)
+ */
+
 int power(int base, int xp){
-    int prod = 1;
-    for (int i = 1; i <= xp; i++)
-        prod = prod * base; 
-    return prod;
+    if (xp < 1)
+        return 1;
+    else
+        return base * power(base, xp - 1);
 }
+
+/* this can also be solved by using a loop as presented in K&R 
+* int power(int base, int xp){
+*     int prod = 1;
+*     for (int i = 1; i <= xp; i++)
+*         prod = prod * base; 
+*     return prod;
+* }
+* */
