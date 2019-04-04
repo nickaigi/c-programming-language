ea8d41221548665758d598343cde3b08fe01a049
Nickson Kaigi <nickaigi@gmail.com>
2019-04-04 13:25:01 +0300

    added helpful debug statements. i thought there was an error in my code

--- chapter_2/any.c
+++ chapter_2/any.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 #define MAX 30
+
 
 int any(char[], char[]);
 
 int main(){
     char s1[MAX] = "blinkenlight";
-    char s2[MAX] = "night";
+    char s2[MAX] = "knight";
     int pos;
     printf("s1:          %s\n", s1);
     printf("s2:          %s\n", s2);
@@ -24,8 +26,9 @@ int any(char s1[], char s2[]){
     int i, j;
     for(i=0; s1[i] != '\0'; i++){
         for(j=0; s2[j] != '\0'; j++){
+            printf("s1[%d]: %c s2[%d]: %c\n", i, s1[i], j, s2[j]);
             if(s1[i] == s2[j])
-                return ++i; // i is zero indexed, so we increment 1
+                return ++i; // i is zero indexed, so we increment i by 1. Note my use of the pre-increment
         }
     }
     return -1;
