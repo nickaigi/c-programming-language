4d7e0f5dbeb61ceb6ba69c3401c5734f6b94b03b
Nickson Kaigi <nickaigi@gmail.com>
2019-08-28 06:20:21 +0300

    added all the calculator code

--- chapter_4/calculator.c
+++ chapter_4/calculator.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
+#include <ctype.h>
 
 
 #define MAXOP 100  /* max size of operand or operator */
 #define NUMBER '0' /* signal that a number was found */
 
+#define MAXVAL 100    /* max dept of val stack */
+
+#define BUFSIZE 100
 
 int getop(char []);
 void push(double);
 double pop(void);
+
+int getch(void);
+void ungetch(int);
+
+int sp = 0;           /* next freee stack position */
+double val[MAXVAL];   /* value stack */
+
+char buf[BUFSIZE];    /* buffer for ungetch */
+int bufp = 0;         /* next free position in buf */
 
 /* reverse Polish calculator */
 int main() {
@@ -47,4 +60,55 @@ int main() {
         }
     }
     return 0;
+}
+
+
+/* push: push f onto value stack */
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+/* getop: get next character or numeric operand */
+int getop(char s[]) {
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;  /* not a number */
+    i = 0;
+    if (isdigit(c))   /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')     /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+int getch(void) {     /* get a (possibly pushed-back) character */
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) { /* push character back on input */
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
 }
