2cbba109ce59b0c85ff3d9f494f2ca757c464c76
Nickson Kaigi <nickaigi@gmail.com>
2019-09-04 12:13:56 +0300

    address arithmetic

---
+++ chapter_5/alloc.c
@@ -0,0 +1,14 @@
+/* Address Arithmetic */
+#define ALLOCSIZE 10000 /* size of available space */
+
+static char allocbuf[ALLOCSIZE];  /* storage for alloc */
+static char *allocp = allocbuf;   /* next free position */
+
+
+char *alloc(int n) {             /* return pointer to n characters */
+    if (allocbuf + ALLOCSIZE - allocp >= n) { /* it fits */
+        allocp += n;
+        return allocp - n; /* old p */
+    } else
+        return 0;
+}
