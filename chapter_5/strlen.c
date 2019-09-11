fe30f25d91d445e56ebdf6e7bce4763ab6793ada
Nickson Kaigi <nickaigi@gmail.com>
2019-09-11 05:12:26 +0300

    added improved strlen function

--- chapter_5/strlen.c
+++ chapter_5/strlen.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int mystrlen(char *);
+int improvedstrlen(char *);
 
 int main() {
     printf("mystrlen(\"hello, world\") = %d\n", mystrlen("hello, world"));
     /* Warning:
      * ISO C++11 does not allow conversion from string literal to 'char *' 
      */
+    printf("improvedstrlen(\"hello, world\") = %d\n", improvedstrlen("hello, world"));
     return 0;
 }
 
@@ -18,4 +20,17 @@ int mystrlen(char *s) {
     for (n = 0; *s != '\0'; s++)
         n++;
     return n;
+}
+
+/* Improved strlen
+ * 
+ * if the pointes p and q point to the members of the same array
+ * and p < q then q - p + 1 is the number of elements from p to q inclusive
+ */
+
+int improvedstrlen(char *s) {
+    char *p = s;
+    while(*p != '\0')
+        p++;
+    return p - s;
 }
