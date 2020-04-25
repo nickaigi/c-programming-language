a87b249c02bb35f48fd8c65794408285b3661865
Nickson Kaigi <nickaigi@gmail.com>
2020-04-25 12:04:30 +0300

    pointers to struct

--- chapter_6/basic_struct.c
+++ chapter_6/basic_struct.c
@@ -53,9 +53,76 @@ struct point makepoint(int x, int y){
     return temp;
 }
 
-/* addpoints: add two points */
+/* addpoints: add two points
+ * structure parameters are passed by value
+ */
 struct point addpoint(struct point p1, struct point p2){
     p1.x += p2.x;
     p1.y += p2.y;
     return p1;
+}
+
+/* ptinrect: return 1 if p in r, 0 if not
+ * - convention: a rectangle includes its left and bottm sides but not its
+ *   top and right sides
+ * - assumes that the rectangle is presented in a standard form where pt1
+ *   coordinates are less than pt2 coordinates.
+ */
+int ptinrect(struct point p, struct rect r){
+    return p.x >= r.pt1.x && p.x < r.pt2.x
+        && p.y >= r.pt1.y && p.y < r.pt2.y;
+}
+
+/* the following function returns a rectangle guaranteed to be in canonical
+ * form
+ */
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+/* canonrect: canonicalize coordinates of rectangle */
+struct rect canonrect(struct rect r){
+    struct rect temp;
+
+    temp.pt1.x = min(r.pt1.x, r.pt2.x);
+    temp.pt1.y = min(r.pt1.y, r.pt2.y);
+
+    temp.pt2.x = min(r.pt1.x, r.pt2.x);
+    temp.pt2.y = min(r.pt1.y, r.pt2.y);
+
+    return temp;
+}
+
+/* - if a large structure is to be passed to a funciton, it is generally more
+ *   efficient to pass a pointer than to copy the whole structure.
+ *
+ *          struct point *pp;
+ *
+ * - says that 'pp' is a pointer to a structure of type 'struct point'.
+ *   if 'pp' points to a 'point' structure, '*pp' is the structure, and
+ *   '(*pp).x' and '(*pp).y' are the members.
+ * - Parentheses are necessary in '(*pp).x' because the precedence of the
+ *   structure member operator '.' is higher than '*'
+ * - The expression '*pp.x' means * (pp.x) which is illegal here because x
+ *   is not a pointer
+ * - Pointers to structures are so frequently used that an alternative notation
+ *   is provided as a shorthand.
+ *   If 'p' is a pointer to a structure, then
+ *
+ *          p->member-of-structure
+ *   refers to the particular member.
+ *  - So we could write instead
+ *          printf("origin is (%d, %d)\n", pp->x, pp->y);
+ */
+
+void demostructpointer(){
+    struct point origin, *pp;
+
+    origin.x = 0;
+    origin.y = 0;
+
+    pp = &origin;
+    printf("origin is (%d, %d)\n", (*pp).x, (*pp).y);
+    /* concise version */
+    printf("origin is (%d, %d)\n", pp->x, pp->y);
 }
