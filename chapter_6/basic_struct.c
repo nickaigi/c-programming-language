d6fd08c32a68b4054ecc240d7fd86e159be7adaf
Nickson Kaigi <nickaigi@gmail.com>
2020-04-25 07:48:32 +0300

    nested struct, some rules

--- chapter_6/basic_struct.c
+++ chapter_6/basic_struct.c
@@ -12,6 +12,20 @@ struct point {
     int y;
 };
 
+/* structures can be nested */
+struct rect {
+    struct point pt1;
+    struct point pt2;
+};
+
+/* legal operations on a structure:
+ *      - copying it
+ *      - assigning to it as a unit
+ *      - taking its address with '&'
+ *      - accessing its members
+ * structures may not be compared
+ */
+
 int main(){
     struct point pt = { 5, 10 };
     printf("pt(%d, %d)\n", pt.x, pt.y);
