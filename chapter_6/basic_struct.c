a778dcb3fdd02cd63ba9efd9ac6e1508b633c145
Nickson Kaigi <nickaigi@gmail.com>
2020-04-25 07:57:37 +0300

    operations on structs

--- chapter_6/basic_struct.c
+++ chapter_6/basic_struct.c
@@ -7,6 +7,9 @@
 
 #include<stdio.h>
 
+#define XMAX 3200
+#define YMAX 3200
+
 struct point {
     int x;
     int y;
@@ -28,6 +31,31 @@ struct rect {
 
 int main(){
     struct point pt = { 5, 10 };
+    struct rect screen;
+    struct point middle;
+    struct point makepoint(int, int);
+
+    screen.pt1 = makepoint(0, 0);
+    screen.pt2 = makepoint(XMAX, YMAX);
+    middle = makepoint((screen.pt1.x + screen.pt2.x)/2,
+                        (screen.pt1.y + screen.pt2.y)/2);
     printf("pt(%d, %d)\n", pt.x, pt.y);
+
     return 0;
+}
+
+/* makepoint: make a point from x and y components */
+struct point makepoint(int x, int y){
+    struct point temp;
+
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
+
+/* addpoints: add two points */
+struct point addpoint(struct point p1, struct point p2){
+    p1.x += p2.x;
+    p1.y += p2.y;
+    return p1;
 }
