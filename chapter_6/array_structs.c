114eac89f86ef360861fbbb5f6f24c6a0ef6afcd
Nickson Kaigi <nickaigi@gmail.com>
2020-04-27 15:55:03 +0300

    added binsearch

--- chapter_6/array_structs.c
+++ chapter_6/array_structs.c
@@ -41,6 +41,26 @@ int main(){
                     keytab[n].count, keytab[n].word);
     return 0;
 }
+
+/* binsearch: find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n){
+    int cond;
+    int low, high, mid;
+    
+    low = 0;
+    high = n - 1;
+    while (low <= high){
+        mid = (low + high)/2;
+        if ((cond = strcmp(word, tab[mid].word)) < 0)
+            high = mid - 1;
+        else if (cond > 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
+
 /* - since the structure keytab contains set of names, it is easiest to make it
  *   an external variable and initialize it once and for all when it is defined.
  * - The initializers are listed in pairs corresponding to the structure
