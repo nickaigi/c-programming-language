73a7af39f2e679590ad5d40b27db8a9f0f1be94a
Nickson Kaigi <nickaigi@gmail.com>
2020-05-07 18:29:30 +0300

    added tnode in full

--- chapter_6/tnode.c
+++ chapter_6/tnode.c
@@ -28,3 +28,42 @@ int main(){
     treeprint(root);
     return 0;
 }
+
+struct tnode *talloc(void);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w){
+    int cond;
+    if (p == NULL) {    /* a new word has arrived */
+        p = talloc();   /* make a new node */
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    } else if ((cond = strcmp(w, p->word)) == 0)
+        p->count++;      /* repeated word */
+    else if (cond < 0)   /* less than into left subtree */
+        p->left = addtree(p->left, w);
+    else                 /* greater than into right subtree */
+        p->right = addtree(p->right, w);
+    return p;
+}
+
+/* treepring: in-order print of tree p */
+void treeprint(struct tnode *p){
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+#include <stdlib.h>
+
+/* talloc: make a tnode */
+struct tnode *talloc(void){
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+/* strdup: copies the string given by its argument into a safe
+ * place, obtained by a call on malloc
+ */
