bd5d49ae2cc8f85879e64ea34ef74a693ac73465
Nickson Kaigi <nickaigi@gmail.com>
2020-05-08 21:23:15 +0300

    putting this test file under vc

--- test.c
+++ test.c
@@ -1,29 +1,20 @@
+#include<stdlib.h>
 #include<stdio.h>
 
 int main(){
-    int i;
-    int arr[10];
-    int n1, n2;
-    int x1, x2;
+    double A[5] = {
+        [0] = 9.0,
+        [1] = 2.9,
+        [4] = 3.E+25,
+        [3] = .00007,
+    };
 
-    n1 = n2 = 5;
+    for (size_t i = 0; i < 5; ++i) {
+        printf("element %zu is %g, \tits square is %g\n",
+                i,
+                A[i],
+                A[i] * A[i]);
+    }
 
-    for (i = 0; i < 10; arr[i] = 1, ++i)
-        ;
-
-    ++arr[0];
-    for (i = 0; i < 10; ++i)
-        printf("%d\n", arr[i]);
-
-    /*
-     * Demonstrates pre-increment and post-increment
-     * x = n++ means asign x to the value of n, then increment n by 1
-     *
-     * x = ++n means increment the value of n by 1, then asign that to x
-     */
-    x1 = n1++;
-    x2 = ++n2;
-    printf("x1 should be 5 and is: %d\n", x1);
-    printf("x2 should be 6 and is: %d\n", x2);
-    return 0;
+    return EXIT_SUCCESS;
 }
