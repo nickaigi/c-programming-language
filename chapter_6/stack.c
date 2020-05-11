95afca5122eac0da4a8455c0aa0b6acab8023c34
Nickson Kaigi <nickaigi@gmail.com>
2020-05-11 22:33:17 +0300

    added stackprint

--- chapter_6/stack.c
+++ chapter_6/stack.c
@@ -10,6 +10,7 @@ struct node* nalloc(void);
 void push(int, struct node**);
 void pop(struct node**);
 void peak(struct node*);
+void stackprint(struct node**);
 
 int main(){
     struct node* top = nalloc();
@@ -17,7 +18,10 @@ int main(){
     for(c = 0; c < 10; c++)
         push(c, &top);
 
+    printf("Peak the top of the stack: ");
     peak(top);
+    printf("Print the entire stack\n");
+    stackprint(&top);
     return 0;
 }
 
@@ -51,8 +55,12 @@ struct node* nalloc(void){
     return (struct node *) malloc(sizeof(struct node));
 }
 
-void stackprint(struct node *head){
-    struct node* current = head;
+/* stackprint: print entire content of stack 
+ * has an error: there a zero at the bottom of the stack, where does that come
+ * from??
+ */
+void stackprint(struct node** head){
+    struct node* current = *head;
     while (current != NULL){
         printf("%d\n", current->data);
         current = current->next;
