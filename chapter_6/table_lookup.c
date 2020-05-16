915df70d548afec416b3bfe0e0453fb75f489b58
Nickson Kaigi <nickaigi@gmail.com>
2020-05-16 23:27:39 +0300

    added function hash

--- chapter_6/table_lookup.c
+++ chapter_6/table_lookup.c
@@ -10,3 +10,25 @@
  *
  * it must be replaced by 1.
  */
+
+struct nlist {           /* table entry: */
+    struct nlist *next;  /* next entry in chain */
+    char *name;          /* defined name */
+    char *defn;          /* replacement text */
+};
+
+
+#define HASHSIZE 101
+
+static struct nlist *hastb[HASHSIZE];  /* pointer table */
+
+/* hash: from hash value for string s */
+unsigned hash(char *s) {
+    unsigned hashval;
+    /* unsinged arithmetic ensures that the hash value is non negative */
+
+    for (hashval = 0; *s != '\0'; s++)
+        hashval = *s + 31 * hashval;
+
+    return hashval % HASHSIZE;
+}
